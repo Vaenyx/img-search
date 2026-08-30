4a6e5b643135291f0f9ae4ff84c9603b47a60270
Vaenyx <242270953+Vaenyx@users.noreply.github.com>
2026-08-30 03:18:44 +0200

    Improved args help msgs

--- src/args.rs
+++ src/args.rs
@@ -4,17 +4,19 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 #[command(author, version, about = "Find occurrences of text in images")]
 pub struct Args {
+    #[arg(help = "Input (case insensitive)")]
     pub input: String,
 
+    #[arg(help = "Search directory to match input against")]
     pub search_directory: Option<PathBuf>,
 
-    #[arg(short, long, help = "Cache directory")]
+    #[arg(short, long, help = "Cache directory to store ocr representations")]
     pub cache_directory: Option<PathBuf>,
 
     #[arg(
         short,
         long,
-        help = "Indexing languages (ISO 639-2)",
+        help = "Indexing languages for ocr (ISO 639-2)",
         default_values_t = ["eng".to_string(), "deu".to_string()]
     )]
     pub languages: Vec<String>,
